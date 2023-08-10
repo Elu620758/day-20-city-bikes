a262bc3f9bb9279ee404a351a87132aefff82f4f
Elu620758 <108872017+Elu620758@users.noreply.github.com>
2023-08-11 01:02:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,18 @@
 const networksContainer = document.getElementById('networks-container');
 
 // Fetch data from the API
-try {
-  fetch('http://api.citybik.es/v2/networks?fields=id,name,href')
-    .then(response => response.json())
-    .then(data => {
-      // Process the response data
-      const networks = data.networks;
 
-      // Create and append network elements to the container
-      networks.forEach(network => {
-        const networkElement = document.createElement('div');
-        networkElement.innerHTML = 
+
+fetch('http://api.citybik.es/v2/networks?fields=id,name,href')
+  .then(response => response.json())
+  .then(data => {
+    // Process the response data
+    const networks = data.networks;
+
+    // Create and append network elements to the container
+    networks.forEach(network => {
+      const networkElement = document.createElement('div');
+      networkElement.innerHTML =
         `<div class="card">     
                 <div class="card-body">
                     <h5 class="card-title">City Bike</h5>
@@ -21,10 +22,7 @@ try {
                     <p class="card-text"><a href="${network.href}" class="btn btn-primary">Go To ${network.name}</a></p>        
                 </div>
         </div>`
-        networksContainer.appendChild(networkElement);
-      });
+      networksContainer.appendChild(networkElement);
     })
-    
-} catch (error) {
-  console.error(error)
-}
+  })
+ .catch("error")
